23d45943fd8f0219ca0c05467dbae35f375ba8f3
mohamedx25 <othmanx23@gmail.com>
2019-01-14 21:52:01 -0500

    Fix to layout updates to header
    I need to figure out how to get rid of the underlines for the listlink

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Footer from './footer'
+import './layout.css'
 
 const ListLink = props =>(
-    <li style={{ display: `inline-block`, marginRight: `1rem`}}>
+    <li style={{ display: `inline-block`, marginRight: `1rem`, textDecoration: 'none'}}>
         <Link to={props.to}>{props.children}</Link>
     </li>
 )
 export default({ children }) =>(
-    <div style= {{ margin: '3rem auto', maxWidth: '100%', backgroundColor: 'none' }}>
-      <header style={{ marginBottom: `1.5rem`}}>
-        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none`}}>
-           <h1>A2Z fitness</h1>
+    <div style= {{ margin: '0 auto', maxWidth: '100%'}} >
+      <header style={{ marginBottom: `0 auto`}} className='header'>
+        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none`}} 
+        className='logo'>
+           <h3>A2Z Fitness</h3>
         </Link>
-        <ul>
-            <ListLink to="/">Home</ListLink>
+        <ul className='nav'>
+            <ListLink to="/" className='link'>Home</ListLink>
             <ListLink to="/about/">About</ListLink>
             <ListLink to="/store/">Store</ListLink>
             <ListLink to="/contact/">Contact</ListLink>
         </ul>
       </header>
-      <Footer />
       {children}
+      <Footer />
+      
     </div>
 )
 
