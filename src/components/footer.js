cbc2973b0325ce875d57f0452a2f654b52516dcf
Mohamed Farih <othmanx23@gmail.com>
2019-01-17 16:01:14 -0500

    added fontawesome
    Added fontawesome using npm. Now need to learn how to add it to the footer successfully with the correct social media icons.

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './layout.css'
 
 
+
 const Footer = (props) => (
     <footer id="footer" className='footer'>
         <section>
             <ul className='icons'>
-                <li><a href='#' className='fa fa-instagram'>Instagram</a></li>
-                <li><a href='#'>Twitter</a></li>
+                <li style= {{color: 'white'}}><a href='#' className='fa fa-instagram'>Instagram</a></li>
+                <li><a href='#' className='fa fa-Twitter'>Twitter</a></li>
+                
             </ul>
         </section>
         <p className='copyright'>&copy; 2019 A2Z Fitness</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
 import React from "react"
 import Layout from '../components/layout'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fab, faCoffee); 
 
 export default() => (
     <Layout>
-        <h1>Hello</h1>
-        <p>my name is amine zaouia</p>
+        <div className='container'>
+            <h1>Hello</h1>
+            <p>my name is amine zaouia</p>
+            <FontAwesomeIcon icon='coffee' />
+            <FontAwesomeIcon icon={['fab', 'google']} />
+        </div>
     </Layout>
 )
