e836e8416b836ce93204df7b77a8cee2ce85a2eb
mohamedx25 <othmanx23@gmail.com>
2019-01-28 20:22:34 -0500

    fix to divs and added bootstrap
    Now need to use bootstrap to make a responsive header and alll the pages responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react"
 import Layout from '../components/layout'
 import { library } from '@fortawesome/fontawesome-svg-core'
-
+import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap-theme.css';
+import { Button } from 'react-bootstrap';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import '../components/index.css'
@@ -21,11 +23,32 @@ export default() => (
                     </div>
                 </div>
             </div>
-            <div className='group-training'>
-                <h1>Group fitness</h1>
-            </div>
             <div className='sports-training'>
-                <h1>Sports Training</h1>
+                <h1>Sports Specialized Training</h1>
+                <p>
+                    A2Z Fitness speciliazes in sports specific 
+                    strength and conditioning training. We train you
+                    to help you excel in your sport doesnt matter the sport.
+                    We focus on specific sports related movement. 
+                </p>
+                <h5>Contact me to schedule an individual training session!</h5>
+                {/* put a button heree. Bootstrap? */}
+                <Button bsStyle='primary' bsSize='large' active>
+                    Contact Me!
+                </Button>                
+
+            </div>
+            <div className='group-training'>
+                <h1>Group Fitness Classes</h1>
+                <p>
+                    I run Group HIIT and bootcamp classes at Signature Fitness! 
+                    {/*add a link to signature fitness*/} 
+                    
+                    I run classes every Tuesday 7 pm, Thursday 7 pm and Fridays at 6 am.
+                </p>
+                <div>
+                    {/*Put the promo video here!  */}
+                </div>
             </div>
             <div className='personal-training'>
                 <h1>Personal Training</h1>
